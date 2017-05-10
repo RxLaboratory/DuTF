25bb54f607db8c4dda5b4b13095f35814b062894
Kevin Masson <contact.kevinmasson@gmail.com>
2017-05-10 20:25:35 +0200

    Try to make the parser work

--- extendscript/Translations tests.jsx
+++ extendscript/Translations tests.jsx
@@ -2,15 +2,21 @@
 #include Dutranslator.jsxinc
 
 Dutranslator.getAvailable("../examples", "Duik_translations_");
-alert(Dutranslator.languages);
-
 
 function check(value, required, message){
     if(value !== required)
     {
         alert(value + " is not equal to " + required + "\n\n" + message);
-        throw new Error("Value not equals");
-     }
-    
+     }    
 }
 
+check(Dutranslator.languages, [
+    ['en', 'English'],
+    ['de', 'Deutsch'],
+    ['es', 'Español'],
+    ['fr', 'Français'],
+    ['pl', 'Polski'],
+    ['zh_HANS', '简体中文']
+], "The languages list is not correct");
+
+alert("Tests end.");
