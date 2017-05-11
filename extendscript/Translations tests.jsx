377f48f48e66dfa74df2977b2b2d74c8d0abdae9
Duduf <git@duduf.com>
2017-05-11 20:11:32 +0200

    json parser tests

--- extendscript/Translations tests.jsx
+++ extendscript/Translations tests.jsx
@@ -3,20 +3,24 @@
 
 Dutranslator.getAvailable("../examples", "Duik_translations_");
 
-function check(value, required, message){
+function check(value, required, message){    
     if(value !== required)
     {
         alert(value + " is not equal to " + required + "\n\n" + message);
-     }    
+     }
+     else
+     {
+        alert(value + " is OK!");
+     }
 }
 
-check(Dutranslator.languages, [
-    ['en', 'English'],
-    ['de', 'Deutsch'],
-    ['es', 'Español'],
-    ['fr', 'Français'],
-    ['pl', 'Polski'],
-    ['zh_HANS', '简体中文']
-], "The languages list is not correct");
+var value = [];
+for (var i = 0 ; i < Dutranslator.languages.length ; i++)
+{
+    value.push(Dutranslator.languages[i].languageId.toString());
+}
+
+//check(value, ['en','de','es','fr','pl','zh_HANS'], "The languages list is not correct");
+alert(value);
 
 alert("Tests end.");
