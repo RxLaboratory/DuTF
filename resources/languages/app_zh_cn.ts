a7a4071082acc8938bf6880d27227fe91a985038
eZio Pan <eziopan@qq.com>
2017-10-15 16:39:53 +0800

    simplified chinese translation

---
+++ resources/languages/app_zh_cn.ts
@@ -0,0 +1,332 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../src/aboutdialog.ui" line="14"/>
+        <source>Duduf Translator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/aboutdialog.ui" line="20"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Duduf Translator&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;version 0.5-Beta&lt;/span&gt;&lt;/p&gt;&lt;p&gt;This tool will soon be part of the Duduf Translator framework for Adobe ExtendScript/JavaScript (and maybe other languages...)&lt;/p&gt;&lt;p&gt;It is a &amp;quot;Beta&amp;quot; version, this means we&apos;re still soon in its development, but it can already be tested.&lt;br/&gt;It is used and tested with Duik 15 and should be included with Duik 16 in a release version.&lt;/p&gt;&lt;p&gt;To use it, you need to open a translation file like the ones included in Duik&lt;br/&gt;(opening the file can be a bit long, be patient!).&lt;br/&gt;Then, don&apos;t forget to update the language code and the language name on the top right of the window.&lt;/p&gt;&lt;p&gt;Duik is available on our website &lt;a href=&quot;https://rainboxprod.coop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://rainboxprod.coop&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Come to github for more information &lt;a href=&quot;https://github.com/Rainbox-dev/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/Rainbox-dev/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Development:&lt;/span&gt;&lt;br/&gt;Nicolas Dufresne&lt;br/&gt;Kevin Masson&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;© 2017 Nicolas Dufresne and Rainbox Productions&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Duduf Translator&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;version 0.5-Beta&lt;/span&gt;&lt;/p&gt;&lt;p&gt;这个工具将很快成为 Duduf Translator for Adobe ExtendScript/JavaScript 的一部分（也可能支持其他语言...）&lt;/p&gt;&lt;p&gt;C&apos;这是&amp;quot;Beta&amp;quot;，这意味着我们还将继续开发它，但它已经可以被用于测试了。&lt;br/&gt;它已经在 Duik 15 上测试了，并应该会随 Dui 16 一同发布。&lt;/p&gt;&lt;p&gt;要使用它，你需要打开一个翻译文件，比如包含在 Duik 中的一个;br/&gt;（打开过程可能会有点长，请耐心等待！）&lt;br/&gt;然后，不要忘记更新窗口右上角的“语言代号”和“语言名称”。&lt;/p&gt;&lt;p&gt;Duik 可以在我们的网站上找到&lt;a href=&quot;https://rainboxprod.coop&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://rainboxprod.coop&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;浏览 Github 获取更多信息&lt;a href=&quot;https://github.com/Rainbox-dev/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://github.com/Rainbox-dev/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;开发：&lt;/span&gt;&lt;br/&gt;Nicolas Dufresne&lt;br/&gt;Kevin Masson&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;© 2017 Nicolas Dufresne et Rainbox Productions&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LanguageWidget</name>
+    <message>
+        <location filename="../../src/languagewidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>表格</translation>
+    </message>
+    <message>
+        <location filename="../../src/languagewidget.ui" line="73"/>
+        <source>Language Code</source>
+        <translation>语言代号</translation>
+    </message>
+    <message>
+        <location filename="../../src/languagewidget.ui" line="92"/>
+        <source>Language name</source>
+        <translation>语言名称</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="23"/>
+        <source>Duduf Translator</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="105"/>
+        <source>Actions</source>
+        <translation>动作</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="110"/>
+        <source>Original</source>
+        <translation>原文</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="115"/>
+        <source>Context</source>
+        <translation>内部序列</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="120"/>
+        <source>Translated</source>
+        <translation>译文</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="125"/>
+        <source>Comment</source>
+        <translation>备注</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="204"/>
+        <source>Generate a translator for </source>
+        <translation>为以下语言创建翻译</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="212"/>
+        <source>Adobe ExtendScript</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="220"/>
+        <source>Generate!</source>
+        <translation>创建！</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="289"/>
+        <location filename="../../src/mainwindow.ui" line="391"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="292"/>
+        <location filename="../../src/mainwindow.ui" line="394"/>
+        <source>Open an existing translation file</source>
+        <translation>打开现有的翻译文件</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="382"/>
+        <source>Ctrl+Alt+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="397"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="421"/>
+        <source>Merge</source>
+        <translation>合并</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="424"/>
+        <source>Ctrl+M</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="301"/>
+        <location filename="../../src/mainwindow.ui" line="406"/>
+        <location filename="../../src/mainwindow.ui" line="409"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="304"/>
+        <source>Save the current translations</source>
+        <translation保存当前翻译</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="307"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="316"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="319"/>
+        <source>Need help? Click here!</source>
+        <translation>需要帮助？点击这里！</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="328"/>
+        <source>Save as...</source>
+        <translation>另存为...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="331"/>
+        <source>Save current translations as...</source>
+        <translation>将当前翻译另存为...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="334"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="346"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="358"/>
+        <location filename="../../src/mainwindow.ui" line="361"/>
+        <source>Tools</source>
+        <translation>工具</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="364"/>
+        <source>Ctrl+T</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="376"/>
+        <source>Export</source>
+        <translation>导出</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.ui" line="379"/>
+        <source>Export...</source>
+        <translation>导出...</translation>
+    </message>
+    <message>
+        <location filename="../../src/mainwindow.cpp" line="105"/>
+        <source>Initializing Dutranslator...</source>
+        <translation>Dutranslator 初始化...</translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesWidget</name>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>表格</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="74"/>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="89"/>
+        <source>Style</source>
+        <translation>风格</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="101"/>
+        <source>Dutranslator default style</source>
+        <translation>默认风格</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="105"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="110"/>
+        <source>System</source>
+        <translation>系统</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="115"/>
+        <source>Custom...</source>
+        <translation>自定义...</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="139"/>
+        <source>Tool Bar</source>
+        <translation>工具栏</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="147"/>
+        <source>Text under icon</source>
+        <translation>图标下文本</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="152"/>
+        <source>Text beside icon</source>
+        <translation>图标侧文本</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="157"/>
+        <source>Icon only</source>
+        <translation>仅图标</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="162"/>
+        <source>Text only</source>
+        <translation>仅文本</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="170"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="178"/>
+        <source>Default (english)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="183"/>
+        <source>Français</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="188"/>
+        <source>Español</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="193"/>
+        <source>简体中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/preferenceswidget.ui" line="198"/>
+        <source>Back</source>
+        <translation>返回</translation>
+    </message>
+</context>
+<context>
+    <name>RowButtonsWidget</name>
+    <message>
+        <location filename="../../src/rowbuttonswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>表格</translation>
+    </message>
+    <message>
+        <location filename="../../src/rowbuttonswidget.ui" line="35"/>
+        <source>Remove row</source>
+        <translation>移除行</translation>
+    </message>
+    <message>
+        <location filename="../../src/rowbuttonswidget.ui" line="49"/>
+        <source>Insert new row after this one</source>
+        <translation>在当前行后插入新行</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWidget</name>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="20"/>
+        <source>Form</source>
+        <translation>表格</translation>
+    </message>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="72"/>
+        <source>Search...</source>
+        <translation>查找...</translation>
+    </message>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="100"/>
+        <source>Original</source>
+        <translation>原文</translation>
+    </message>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="107"/>
+        <source>Translated</source>
+        <translation>译文</translation>
+    </message>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="114"/>
+        <source>Comment</source>
+        <translation>备注</translation>
+    </message>
+    <message>
+        <location filename="../../src/searchwidget.ui" line="121"/>
+        <source>Case Sensitive</source>
+        <translation>大小写敏感</translation>
+    </message>
+</context>
+</TS>
