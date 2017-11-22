1fa38ccf16f2671d8b3a221a5212dd71ef109f62
Kevin Masson <contact.kevinmasson@gmail.com>
2017-11-22 14:41:42 +0100

    Use pretty name to manage languages

--- extendscript/Translations tests.jsx
+++ extendscript/Translations tests.jsx
@@ -1,28 +1,35 @@
 ﻿//gets the new translation framework
 #include Dutranslator.jsxinc
 
+var output = "";
+
+
 Dutranslator.settings.folder =  File($.fileName).path + "/../examples/";
 Dutranslator.settings.prefix =  "Duik_translations_";
 Dutranslator.settings.name = "duik";
 
+var start = new Date().getTime();
 Dutranslator.getAvailable();
+var end = new Date().getTime();
+output += "GetAvailable took " + (end- start)+ " ms\n";
 
 
 
 
 
-var output = "";
-output = "Available languages: \n";
-for(var langId in Dutranslator.languages){   
-    output += langId + " " + Dutranslator.languages[langId]["name"] + " " + Dutranslator.languages[langId]["file"]  + "\n";
-}
-output += "\n\n";
+var names = Dutranslator.getPrettyNames();
+output += "Available languages: \n" + names + "\n";
 
-for(var langId in Dutranslator.languages){   
-    output += "Setting lang to " + langId + "\n";
-    var status = Dutranslator.setLanguage(langId);
-    if(status != 0) output += "Setting the lang to " + langId + " went wrong, error code: " + status + "\n";
-    else output += "Translation in " + langId + " of Launch Duik is \n   " + tr("Launch Duik") + "\n";       
+
+for(var i = 0; i < names.length; i++)
+{
+    output += "Setting lang to " + names[i] + "\n";
+    start = new Date().getTime();
+    var status = Dutranslator.setPrettyLanguage(names[i]);
+    end = new Date().getTime();
+    output += "setPrettyLanguage took " + (end - start) + " ms\n";
+    if(status != 0) output += "Setting the lang to " + names[i] + " went wrong, error code: " + status + "\n";
+    else output += "Translation in " + names[i] + " of Launch Duik is \n   " + tr("Launch Duik") + "\n";       
     output += "--------\n";
 }
 
