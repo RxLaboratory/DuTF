5f34859355f75efd878716be3ccfcfc539c02902
Kevin Masson <contact.kevinmasson@gmail.com>
2017-11-22 13:47:22 +0100

    js: Update js dutranslator according to new design

--- extendscript/Translations tests.jsx
+++ extendscript/Translations tests.jsx
@@ -3,6 +3,7 @@
 
 Dutranslator.settings.folder =  File($.fileName).path + "/../examples/";
 Dutranslator.settings.prefix =  "Duik_translations_";
+Dutranslator.settings.name = "duik";
 
 Dutranslator.getAvailable();
 
@@ -16,9 +17,11 @@ output = "Available languages: " + Dutranslator.languages + "\n\n";
 for(var i = 0; i < Dutranslator.languages.length; i++){
     
     var langId = Dutranslator.languages[i];
-    Dutranslator.setLanguage(langId);
-    
-    output += "Translation in " + langId + " of Open General Preferences is " + tr("Open General Preferences") + "\n";       
+    output += "Setting lang to " + langId + "\n";
+    var status = Dutranslator.setLanguage(langId);
+    if(status != 0) output += "Setting the lang to " + langId + " went wrong, error code: " + status + "\n";
+    else output += "Translation in " + langId + " of Launch Duik is \n   " + tr("Launch Duik") + "\n";       
+    output += "--------\n";
 }
 
 output += "\n\nTest ends.";
